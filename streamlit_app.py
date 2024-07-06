7ada838bec44586943f6d925c3b7c6f5569aa1fd
sattyajeet0322 <sattyajeet14@gmail.com>
2024-07-06 23:12:03 +0530

    Update streamlit_app.py
    rw.7 .json

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 # new section to display fruityvice nutrition information 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
 # Write directly to the app
