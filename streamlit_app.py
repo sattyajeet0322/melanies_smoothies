e7d8ba9df23647c18ed9d9806c4f1f59b5f51eaa
sattyajeet0322 <sattyajeet14@gmail.com>
2024-07-06 23:19:21 +0530

    Update streamlit_app.py
    removed comments.. and cut two rows in for loop. fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
                fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width= True)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-# new section to display fruityvice nutrition information 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width= True)
 
 
@@ -36,7 +34,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+            ingredients_string += fruit_chosen + ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width= True)
 
     #st.write(ingredients_string)
 
