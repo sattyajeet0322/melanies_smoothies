5095cde97c5d1cb17b9a79999fc319ac5b325386
sattyajeet0322 <sattyajeet14@gmail.com>
2024-07-06 23:14:31 +0530

    Update streamlit_app.py
    fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width= True)

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ from snowflake.snowpark.functions import col
 # new section to display fruityvice nutrition information 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width= True)
 
 
 # Write directly to the app
